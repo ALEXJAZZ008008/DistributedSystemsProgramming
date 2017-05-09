0b1df82327ccc08b69c529280e1fbbdd64422b8b
483446 <483446@7700f37f-d485-3e40-9b60-5bcdc4074596>
2017-05-09 02:48:00 +0000

    Started ACW... Don't judge

---
+++ Distributed Systems Programming/ACW_08346_483446/ACW_08346_483446_Server/Server.cs
@@ -0,0 +1,45 @@
+﻿using ACW_08346_483446_ServiceLibrary;
+using System;
+using System.ServiceModel;
+using System.Threading;
+
+namespace ACW_08346_483446_Server
+{
+    class Server
+    {
+        static void Main(string[] args)
+        {
+            InitialiseServer();
+
+            OpenServer();
+        }
+
+        private static void InitialiseServer()
+        {
+            Service.InitialiseServer();
+        }
+
+        private static void OpenServer()
+        {
+            ServiceHost serviceHost = new ServiceHost(typeof(Service));
+
+            serviceHost.Open();
+            
+            RunServer(serviceHost);
+        }
+
+        private static void RunServer(ServiceHost serviceHost)
+        {
+            Console.WriteLine("Server running...");
+
+            CloseServer(serviceHost);
+        }
+
+        private static void CloseServer(ServiceHost serviceHost)
+        {
+            Thread.Sleep(9000);
+
+            serviceHost.Close();
+        }
+    }
+}
