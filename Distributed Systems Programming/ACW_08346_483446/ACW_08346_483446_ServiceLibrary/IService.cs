87a36441d46f7bbd9566f6f71655998e9e62d2d0
Alexander Whitehead <alexandercwhitehead@gmail.com>
2017-07-17 17:12:09 +0000

    To GIT

--- Distributed Systems Programming/ACW_08346_483446/ACW_08346_483446_ServiceLibrary/IService.cs
+++ Distributed Systems Programming/ACW_08346_483446/ACW_08346_483446_ServiceLibrary/IService.cs
@@ -2,28 +2,36 @@
 
 namespace ACW_08346_483446_ServiceLibrary
 {
+    //This is the interface for the server
     [ServiceContract]
     public interface IService
     {
+        //This is the interface item for the HELLO method of the server
         [OperationContract]
         string HELLO(int id);
 
+        //This is the interface item for the SORT method of the server
         [OperationContract]
-        string SORT(int number, string[] value);
+        string SORT(string[] value);
 
+        //This is the interface item for the PUBKEY method of the server
         [OperationContract]
-        string PUBKEY();
+        byte[][] PUBKEY();
 
+        //This is the interface item for the ENC method of the server
         [OperationContract]
-        void ENC(string message);
+        void ENC(byte[] message);
 
+        //This is the interface item for the SHA1 method of the server
         [OperationContract]
         string SHA1(string message);
 
+        //This is the interface item for the SHA256 method of the server
         [OperationContract]
         string SHA256(string message);
 
+        //This is the interface item for the SIGN method of the server
         [OperationContract]
-        string SIGN(string message);
+        byte[] SIGN(string message);
     }
 }
